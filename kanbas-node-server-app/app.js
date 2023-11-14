d921249454b037bb5ea4e443fbf2d67dd2cb9095
tarifkhan <tarif.ny@gmail.com>
2023-11-13 22:57:37 -0500

    Courses

--- kanbas-node-server-app/app.js
+++ kanbas-node-server-app/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
+import CourseRoutes from "./Courses/routes.js";
 
 const app = express();
 app.use(express.json());
@@ -9,4 +10,5 @@ app.use(express.json());
 app.use(cors());
 Lab5(app);
 Hello(app);
+CourseRoutes(app);
 app.listen(4000);
