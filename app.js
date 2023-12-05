3937d1c873e5ad3e266a59c264c79f83e220b1e2
tarifkhan <tarif.ny@gmail.com>
2023-12-04 22:18:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -28,13 +28,13 @@ app.use(session(sessionOptions));
 
 app.use(express.json());
 app.use(express.json());
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: process.env.FRONTEND_URL,
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+// app.use(cors());
 Lab5(app);
 Hello(app);
 CourseRoutes(app);
