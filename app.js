95cd67408567748087e72fe56de4d5252c9af820
tarifkhan <tarif.ny@gmail.com>
2023-12-04 22:06:10 -0500

    adsad

--- app.js
+++ app.js
@@ -28,12 +28,13 @@ app.use(session(sessionOptions));
 
 app.use(express.json());
 app.use(express.json());
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: process.env.FRONTEND_URL,
+//   })
+// );
+app.use(cors());
 Lab5(app);
 Hello(app);
 CourseRoutes(app);
