6e9eb0044da00f3c36b045654fb685e58d38fe53
tarifkhan <tarif.ny@gmail.com>
2023-12-04 20:16:38 -0500

    mongo

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ import CourseRoutes from "./Courses/routes.js";
 import ModuleRoutes from "./Modules/routes.js";
 import "dotenv/config";
 import mongoose from "mongoose";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import UserRoutes from "./Users/routes.js";
 import session from "express-session";
-
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 const sessionOptions = {
   secret: "any string",
